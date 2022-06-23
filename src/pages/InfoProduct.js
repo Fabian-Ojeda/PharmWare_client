1575d95f72f23a0de6959c5bd5d5af0590fde223
Fabian-Ojeda <fabian,ojeda01@uptc.edu.co>
2022-06-22 22:16:26 -0500

    Agregación de la interfaz de información de producto

--- src/pages/InfoProduct.js
+++ src/pages/InfoProduct.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
+import FormHandleSearchInfoProduct from "../Components/FormHandleSearchInfoProduct";
 
 const InfoProduct = () => {
+
+    const [product, setProduct] = useState({name:'Dodge Challenger srt Demon', price:590000000, unitSale:'carro', ubication:'F-5'})
+
     return(
-        <h1>Esto es la información de producto</h1>
+        <div className={'container'}>
+            <div>
+                <h1 className='TitleSection'>Información de productos</h1>
+            </div>
+            {/*Panel de busqueda manual*/}
+            <div>
+                <FormHandleSearchInfoProduct/>
+            </div>
+            <div className={'col mt-5'}>
+                <div className={'row'}>
+                    <h1>Nombre: {product.name}</h1>
+                </div>
+                <div className={'row mt-2'}>
+                    <h1>Precio: ${product.price}</h1>
+                </div>
+                <div className={'row mt-2'}>
+                    <h1>Unidad de venta: {product.unitSale}</h1>
+                </div>
+                <div className={'row mt-2'}>
+                    <h1>Ubicación: {product.ubication}</h1>
+                </div>
+            </div>
+        </div>
     )
 }
 
